fbb4cef2e9b759de6707b0c103afd932f76d00ea
ChDan1el <danielchristianolima@gmail.com>
2026-02-25 22:35:52 -0300

    Create Exercício 1.java

---
+++ Exercício 1.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    float horasTotais = 3;
+    int hora1 = 10;
+    int hora2 = 4;
+    int hora3 = 2;
+    
+    int soma = hora1+hora2+hora3;
+    float media= soma/horasTotais;
+    
+     System.out.println("Numero total de lanches consumidos: "+soma);
+     System.out.print("Media de lanche consumido por hora: "+media);
+    }
+}
