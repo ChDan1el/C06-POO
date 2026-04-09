a1541e48acc6f4649df08cbd79d1d9ab87bc5851
ChDan1el <danielchristianolima@gmail.com>
2026-04-08 21:49:46 -0300

    Feito

---
+++ Aula-03/Exercicio6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Random rand = new Random();
+
+        int x = rand.nextInt(10)+1;
+        System.out.print("Tente acertar um número entre 1 e 10: ");
+        int numero = teclado.nextInt();
+
+        while (numero != x) {
+            System.out.print("Tente acertar um número entre 1 e 10: ");
+            numero = teclado.nextInt();
+        }
+        System.out.println("ACERTOU!!");
+        System.out.println("O número era " + x);
+    }
+}
