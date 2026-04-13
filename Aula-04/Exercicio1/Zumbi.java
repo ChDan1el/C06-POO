fce04dfb1d65562f9ef005856bda05fe16983601
ChDan1el <danielchristianolima@gmail.com>
2026-04-13 18:43:44 -0300

    Create Zumbi.java

---
+++ Aula-04/Exercicio1/Zumbi.java
@@ -0,0 +1,13 @@
+public class Zumbi {
+    String nome;
+    int fome;
+    float velocidade;
+    float inteligencia;
+
+    void Comer(){
+        System.out.println("Comendo");
+    }
+    void Correr(){
+        System.out.println("Correndo");
+    }
+}
