5a315340a47403b36ef1038b9c8257c5787889ac
ChDan1el <danielchristianolima@gmail.com>
2026-04-15 19:05:11 -0300

    Exercicio concluido

---
+++ Aula-04/Exercicio5/Arma.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Arma {
+    public String nome;
+    public int poder;
+    public int resistencia;
+    public String descricao;
+
+    public void mostraInfoArma(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Poder: " + poder);
+        System.out.println("Resistencia: " + resistencia);
+        System.out.println("Descricao: " + descricao);
+    }
+}
