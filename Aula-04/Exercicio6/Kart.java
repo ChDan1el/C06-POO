d56b462444f31c965106969c18576d1d2d06c277
ChDan1el <danielchristianolima@gmail.com>
2026-04-15 21:04:47 -0300

    Create Kart.java

---
+++ Aula-04/Exercicio6/Kart.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Kart {
+    public String nome;
+    public Piloto piloto;
+
+    public Kart(){
+        Motor motor = new Motor();
+
+        motor.cilindradas = "100";
+        motor.velocidadeMaxima = 150;
+    }
+
+    public void pular(){
+        System.out.println("O "+this.nome+" deu um pulo");
+    }
+
+    public void soltarTurbo(){
+        System.out.println(this.nome+" ATIVOU O TURBO!!");
+    }
+
+    public void fazerDrift(){
+        System.out.println(this.nome+" fez drift");
+    }
+}
