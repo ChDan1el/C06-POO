d46dd344b7729c24eb2bf1d5574610749edb448a
ChDan1el <danielchristianolima@gmail.com>
2026-04-15 21:05:31 -0300

    Create Piloto.java

---
+++ Aula-04/Exercicio6/Piloto.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Piloto {
+    public String nome;
+    public boolean vilao;
+
+    public void soltarSuperPoder(){
+        System.out.println(this.nome+" SOLTOU UM PODER!!");
+    }
+}
