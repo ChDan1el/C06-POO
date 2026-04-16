f9d94e48db2c1de72e940f664a9300bc74e01764
ChDan1el <danielchristianolima@gmail.com>
2026-04-15 21:04:30 -0300

    Create Main.java

---
+++ Aula-04/Exercicio6/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Piloto piloto1 = new Piloto();
+        Piloto piloto2 = new Piloto();
+        Kart kart1 = new Kart();
+        Kart kart2 = new Kart();
+
+        piloto1.nome = "Mario do Armario";
+        kart1.nome = "ROLIMA";
+        piloto2.nome = "Senhor Borracha";
+        kart2.nome = "TOMADO";
+        piloto2.vilao = true;
+
+        piloto2.soltarSuperPoder();
+        kart1.pular();
+        kart2.fazerDrift();
+        kart2.soltarTurbo();
+    }
+}
