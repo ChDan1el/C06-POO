9eb7cf856fe60414798ce4016bf5f5417332820a
ChDan1el <danielchristianolima@gmail.com>
2026-04-15 21:20:38 -0300

    Corrigido, kart pertence ao piloto

--- Aula-04/Exercicio6/Main.java
+++ Aula-04/Exercicio6/Main.java
@@ -9,9 +9,13 @@ public class Main {
 
         piloto1.nome = "Mario do Armario";
         kart1.nome = "ROLIMA";
+        
         piloto2.nome = "Senhor Borracha";
         kart2.nome = "TOMADO";
         piloto2.vilao = true;
+
+        kart1.piloto = piloto1;
+        kart2.piloto = piloto2;
 
         piloto2.soltarSuperPoder();
         kart1.pular();
