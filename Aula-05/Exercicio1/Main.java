250d00cc5afbba61c48fb9be11131e67ee4376f3
ChDan1el <danielchristianolima@gmail.com>
2026-04-20 10:55:29 -0300

    Update Main.java

--- Aula-05/Exercicio1/Main.java
+++ Aula-05/Exercicio1/Main.java
@@ -1,1 +1,32 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Banda banda = new Banda();
+        banda.nome = "SABATICOS";
+        banda.genero = "UDELE";
+
+        Musica musica1 = new Musica();
+        musica1.nome = "ESCARNIO";
+        Musica musica2 = new Musica();
+        musica2.nome = "SINGELA";
+
+        Membro membro1 = new Membro();
+        membro1.nome = "Robson";
+        Membro membro2 = new Membro();
+        membro2.nome = "Sabrina";
+
+        Empresario empresario = new Empresario();
+        empresario.nome = "Alan Jesus";
+        empresario.cnpj = "SONEGADOR";
+
+        banda.addMusicaNova( musica1);
+        banda.addMusicaNova( musica2);
+        banda.addMembroNovo(membro1);
+        banda.addMembroNovo(membro2);
+        banda.addEmpresario(empresario);
+
+        banda.mostraInfo();
+    }
+}
 
