fa614535c48df0eeed12f6fe48964731b0221ca9
ChDan1el <danielchristianolima@gmail.com>
2026-04-20 11:34:33 -0300

    Create Matematica.java

---
+++ Aula-06/br/inatel/poo/matematica/Matematica.java
@@ -0,0 +1,14 @@
+package br.inatel.poo.matematica;
+
+public class Matematica {
+    int soma(int x, int y){
+        return x+y;
+    }
+    int subtracao(int x, int y){
+        return x-y;
+    }
+
+    double divisao(int x, int y){
+        return x/y;
+    }
+}
